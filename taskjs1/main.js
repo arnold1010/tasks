6f55c9fa363dfd1e47d40711285ca3be7221ef3f
Aryana <aryana-zarifi@mail.ru>
2020-11-12 21:42:56 +0300

    исправила замечания

--- taskjs1/main.js
+++ taskjs1/main.js
@@ -2,27 +2,20 @@ var a = prompt("Введите A:");
 var b = prompt("Введите B:");
 var c = prompt("Введите C:");
 
-var result = Equation(a, b, c);
+var numbers = equation(a, b, c);
 
-function Equation(a, b, c) {
+function equation(a, b, c) {
     let result = [];
     let D = b * b - 4 * a * c;
     if(D < 0){
-       return result = console.log ("Нет корней");
-    }
-    if(D == 0){
+       result = "Нет корней";
+    } else if(D === 0){
         x = (-b + Math.sqrt(D)) / (2 * a);
-        return [x];
-    }   
-    else if(D > 0){
+        result = [x];
+    } else if(D > 0){
         x1 = (-b + Math.sqrt(D)) / (2 * a);
         x2 = (-b - Math.sqrt(D)) / (2 * a);
-        return [x1, x2];
-    }
+        result = [x1, x2];
+    } return result;
 };
-console.log (result);
-
-
-
-
-
+console.log (numbers);
