e14480f4df2ae494a65694be4182a6f502298b28
Aryana <aryana-zarifi@mail.ru>
2020-11-22 21:18:46 +0300

    завершающие исправления по уравнению

--- taskjs1/main.js
+++ taskjs1/main.js
@@ -1,21 +1,22 @@
-var a = prompt("Введите A:");
-var b = prompt("Введите B:");
-var c = prompt("Введите C:");
+let a = prompt("Введите A:");
+let b = prompt("Введите B:");
+let c = prompt("Введите C:");
 
-var numbers = equation(a, b, c);
+let result = equation(a, b, c);
 
 function equation(a, b, c) {
     let result = [];
     let D = b * b - 4 * a * c;
-    if(D < 0){
-       result = "Нет корней";
-    } else if(D === 0){
+    if(D < 0) {
+       result = undefined ;
+    } else if(D === 0) {
         x = (-b + Math.sqrt(D)) / (2 * a);
         result = [x];
-    } else if(D > 0){
+    } else if(D > 0) {
         x1 = (-b + Math.sqrt(D)) / (2 * a);
         x2 = (-b - Math.sqrt(D)) / (2 * a);
         result = [x1, x2];
-    } return result;
+    } 
+    return result;
 };
-console.log (numbers);
+console.log (result);
